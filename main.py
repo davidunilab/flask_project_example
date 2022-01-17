f80d0d86a80b0fdb309d2630634841b1db5f461d
David <davidchincharashvili@gmail.com>
2022-01-17 21:54:09 +0400

    added sqlite queries

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 # pip install -r requirements.txt # install
 # pip freeze > requirements.txt # create file
+# https://docs.python.org/3/library/sqlite3.html
+
 from flask import Flask
 from flask_restful import Resource, Api
+import sqlite3
+con = sqlite3.connect('identifier.sqlite',  check_same_thread=False)
+cur = con.cursor()
+
+# cur.execute("SELECT * FROM students where `name` = 'Arsen'")
+# print(cur.fetchall())
+
+# cur.execute("Update students set name = ? where id = ?",('Gvanca', 5))
+# con.commit()
+
+# cur.execute("Delete from students where  id in (?, ?)", (4, 6))
+# con.commit()
+# CRUD
+
+# cur.executemany("insert InTo students (id, name, course, age) values (?,?,?,?)", [(9, 'Mariami', '', 21), (10, 'Arsen', '?', 21)])
+# con.commit()
+
+
+
+
 
 app = Flask(__name__)
 api = Api(app)
+# mysite.ge/
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class Students(Resource):
+    def get(self, student_id):
+        print(type(student_id))
+        print(student_id)
 
-    def post(self, name):
-        return {"name": name}
+        cur.execute("SELECT * FROM students where id = ?", (student_id,))
+        student = cur.fetchone()
+        print(student)
+        return student
 
-    def put(self, name):
-        return {"name": name}
-
-    def delete(self, name):
-        return {"name": name}
-
-
-api.add_resource(HelloWorld, '/read', endpoint="get")
-api.add_resource(HelloWorld, '/save/<string:name>', endpoint="post")
-# api.add_resource(HelloWorld, '/edit/<string:name>', endpoint="put")
-# api.add_resource(HelloWorld, '/remove/<string:name>', endpoint="delete")
+api.add_resource(Students, '/read/<int:student_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
